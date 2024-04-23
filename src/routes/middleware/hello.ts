8931ed6b31c181d77cc920f085383bb26832893d
marcosrjjunior <marcosrjjunior@gmail.com>
2024-04-23 09:30:05 -0300

    updating type dependency and tweak

--- src/routes/middleware/hello.ts
+++ src/routes/middleware/hello.ts
@@ -2,7 +2,8 @@ import type { MiddlewareHandler } from 'hono'
 // import { HTTPException } from 'hono/http-exception'
 
 export const hello = (message: string = 'Hello!'): MiddlewareHandler => {
-  return async (c, next) => {
+  return async (_, next) => {
+    // c, next
     await next()
     console.log('hello middleware', message)
     // c.res.headers.append('X-Message', message)
