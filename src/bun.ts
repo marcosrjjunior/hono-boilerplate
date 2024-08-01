0e7a78a39e18a123f4b1730b0070054ed1dfa697
marcosrjjunior <marcosrjjunior@gmail.com>
2024-08-01 20:43:40 +1000

    bump up dependencies

--- src/bun.ts
+++ src/bun.ts
@@ -1,6 +1,8 @@
 import { Hono } from 'hono'
 import { csrf } from 'hono/csrf'
 
+import { ZodError } from 'zod'
+
 import { routes } from './routes'
 
 const app = new Hono()
@@ -17,6 +19,10 @@ app.onError((error, c) => {
   //     function: '[FILENAME:FUNCTIONAME]',
   //   },
   // })
+
+  if (error instanceof ZodError) {
+    return c.json({ error, message: error.message }, { status: 403 })
+  }
 
   console.error(`${error}`)
   return c.json(
--- src/node.ts
+++ src/node.ts
@@ -2,6 +2,7 @@ import { Hono } from 'hono'
 import { csrf } from 'hono/csrf'
 
 import { serve } from '@hono/node-server'
+import { ZodError } from 'zod'
 
 import { routes } from './routes'
 
@@ -19,6 +20,10 @@ app.onError((error, c) => {
   //     function: '[FILENAME:FUNCTIONAME]',
   //   },
   // })
+
+  if (error instanceof ZodError) {
+    return c.json({ error, message: error.message }, { status: 403 })
+  }
 
   console.error(`${error}`)
   return c.json(
